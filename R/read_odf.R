3c3ca66054fffc1870ee37e9a686e69a495f05d9
Tom Hartl <thartl@diw.de>
2025-02-11 15:40:01 +0000

    version 2.1.3

--- R/read_odf.R
+++ R/read_odf.R
@@ -125,22 +125,22 @@ read_odf  <-  function(file,
   
   
   #read xml from zipped folder
-  metadata <- read_xml(x = unz(file, "metadata.xml"), encoding = "UTF-8")
+  metadata <- xml2::read_xml(x = unz(file, "metadata.xml"), encoding = "UTF-8")
   
   #Extract study name
-  study_metadata <- xml_children(metadata)[grep("<stdyDscr>",
-                                                xml_children(metadata))]
-  study_metadata <- xml_children(study_metadata)[grep("<citation>",
-                                                      xml_children(
+  study_metadata <- xml2::xml_children(metadata)[grep("<stdyDscr>",
+                                                xml2::xml_children(metadata))]
+  study_metadata <- xml2::xml_children(study_metadata)[grep("<citation>",
+                                                      xml2::xml_children(
                                                         study_metadata))]
-  study_metadata <- xml_children(study_metadata)[grep("<titlStmt>",
-                                                      xml_children(
+  study_metadata <- xml2::xml_children(study_metadata)[grep("<titlStmt>",
+                                                      xml2::xml_children(
                                                         study_metadata))]
-  study_metadata <- xml_children(study_metadata)[grep("<titl>",
-                                                      xml_children(
+  study_metadata <- xml2::xml_children(study_metadata)[grep("<titl>",
+                                                      xml2::xml_children(
                                                         study_metadata))]
-  if (length(xml_text(study_metadata)) > 0) {
-    study <- xml_text(study_metadata)
+  if (length(xml2::xml_text(study_metadata)) > 0) {
+    study <- xml2::xml_text(study_metadata)
   }  else {
     study <- ""
   }
@@ -148,47 +148,47 @@ read_odf  <-  function(file,
   attr(data, "study") <- study
   
   #Extract dataset description
-  dataset_metadata <- xml_children(metadata)[grep("<fileDscr>",
-                                                  xml_children(metadata))]
-  dataset_descrsub <- xml_children(
-    xml_children(dataset_metadata)[
-      xml_name(xml_children(dataset_metadata)) == "fileTxt"])
+  dataset_metadata <- xml2::xml_children(metadata)[grep("<fileDscr>",
+                                                  xml2::xml_children(metadata))]
+  dataset_descrsub <- xml2::xml_children(
+    xml2::xml_children(dataset_metadata)[
+      xml2::xml_name(xml2::xml_children(dataset_metadata)) == "fileTxt"])
   
   #get dataset name study
-  filename  <-  xml_text(dataset_descrsub[
-    xml_name(dataset_descrsub) == "fileName"])
+  filename  <-  xml2::xml_text(dataset_descrsub[
+    xml2::xml_name(dataset_descrsub) == "fileName"])
   attr(data, "name") <- filename
   #get dataset descriptions
   dataset_descriptions <- dataset_descrsub[
-    xml_name(dataset_descrsub) == "fileCont"]
+    xml2::xml_name(dataset_descrsub) == "fileCont"]
   for (description in dataset_descriptions){
     if (languages == "all" ||
-        xml_attr(description, attr = "lang") %in% languages) {
-      if (length(xml_text(description)) > 0)
+        xml2::xml_attr(description, attr = "lang") %in% languages) {
+      if (length(xml2::xml_text(description)) > 0)
         attr(data, paste0("description_",
-                          xml_attr(description, attr = "lang"))) <-
-          xml_text(description)
+                          xml2::xml_attr(description, attr = "lang"))) <-
+          xml2::xml_text(description)
       else attr(data, paste0("description_",
-                             xml_attr(description, attr = "lang"))) <- ""
+                             xml2::xml_attr(description, attr = "lang"))) <- ""
     }
   }
   #get dataset labels
-  dataset_labels <- xml_children(xml_children(
-    dataset_descrsub[xml_name(dataset_descrsub) == "fileCitation"]))
+  dataset_labels <- xml2::xml_children(xml2::xml_children(
+    dataset_descrsub[xml2::xml_name(dataset_descrsub) == "fileCitation"]))
   for (label in dataset_labels){
     if (languages[1] == "all" ||
-        xml_attr(label, attr = "lang") %in% languages) {
-      if (length(xml_text(label)) > 0)
-        attr(data, paste0("label_", xml_attr(label, attr = "lang"))) <-
-          xml_text(label)
-      else attr(data, paste0("label_", xml_attr(label, attr = "lang")))  <- ""
+        xml2::xml_attr(label, attr = "lang") %in% languages) {
+      if (length(xml2::xml_text(label)) > 0)
+        attr(data, paste0("label_", xml2::xml_attr(label, attr = "lang"))) <-
+          xml2::xml_text(label)
+      else attr(data, paste0("label_", xml2::xml_attr(label, attr = "lang")))  <- ""
     }
   }
   #Get dataset url
-  url_node <- xml_children(xml_children(dataset_metadata)[
-    xml_name(xml_children(dataset_metadata)) == "notes"])
-  if (length(xml_attr(url_node, attr = "URI")) > 0) {
-    attr(data, "url") <- xml_attr(url_node, attr = "URI")
+  url_node <- xml2::xml_children(xml2::xml_children(dataset_metadata)[
+    xml2::xml_name(xml2::xml_children(dataset_metadata)) == "notes"])
+  if (length(xml2::xml_attr(url_node, attr = "URI")) > 0) {
+    attr(data, "url") <- xml2::xml_attr(url_node, attr = "URI")
   } else {
     attr(data, "url") <- ""
   }
@@ -196,42 +196,42 @@ read_odf  <-  function(file,
   
   
   #Extract variable description
-  variable_metadata <- xml_children(
-    xml_children(metadata)[grep("<dataDscr>", xml_children(metadata))])
+  variable_metadata <- xml2::xml_children(
+    xml2::xml_children(metadata)[grep("<dataDscr>", xml2::xml_children(metadata))])
   #Loop over MEtadata for each variable to extract
   for (var in variable_metadata){
-    varname <- xml_attr(var, attr = "name")
+    varname <- xml2::xml_attr(var, attr = "name")
     if (varname %in% colnames(data)) {
       #Get variable name
-      attr(data[[varname]], "name") <- xml_attr(var, attr = "name")
+      attr(data[[varname]], "name") <- xml2::xml_attr(var, attr = "name")
       #Get variable labels
-      for (label in xml_children(var)[xml_name(xml_children(var)) == "labl"]) {
-        if (languages[1] == "all" || xml_attr(
+      for (label in xml2::xml_children(var)[xml2::xml_name(xml2::xml_children(var)) == "labl"]) {
+        if (languages[1] == "all" || xml2::xml_attr(
           label, attr = "lang") %in% languages) {
-          if (length(xml_text(label)) > 0)
+          if (length(xml2::xml_text(label)) > 0)
             attr(data[[varname]], paste0(
-              "label_", xml_attr(label, attr = "lang"))) <- xml_text(label)
+              "label_", xml2::xml_attr(label, attr = "lang"))) <- xml2::xml_text(label)
           else attr(data[[varname]], paste0(
-            "label_", xml_attr(label, attr = "lang"))) <- ""
+            "label_", xml2::xml_attr(label, attr = "lang"))) <- ""
         }
       }
       
       #Get variable descriptions
-      for (descr in xml_children(var)[xml_name(xml_children(var)) == "txt"]) {
+      for (descr in xml2::xml_children(var)[xml2::xml_name(xml2::xml_children(var)) == "txt"]) {
         if (languages[1] == "all" ||
-            xml_attr(descr, attr = "lang") %in% languages) {
-          if (length(xml_text(descr)) > 0)
+            xml2::xml_attr(descr, attr = "lang") %in% languages) {
+          if (length(xml2::xml_text(descr)) > 0)
             attr(data[[varname]], paste0(
-              "description_", xml_attr(descr, attr = "lang"))) <-
-              xml_text(descr)
+              "description_", xml2::xml_attr(descr, attr = "lang"))) <-
+              xml2::xml_text(descr)
           else attr(data[[varname]], paste0(
-            "description_", xml_attr(descr, attr = "lang"))) <- ""
+            "description_", xml2::xml_attr(descr, attr = "lang"))) <- ""
         }
       }
       
       #Get variable type
-      type <- xml_attr(xml_children(var)[
-        xml_name(xml_children(var)) == "varFormat"], attr = "type")
+      type <- xml2::xml_attr(xml2::xml_children(var)[
+        xml2::xml_name(xml2::xml_children(var)) == "varFormat"], attr = "type")
       if (length(type > 0)) {
         attr(data[[varname]], "type") <- type
       } else {
@@ -239,43 +239,43 @@ read_odf  <-  function(file,
       }
       
       #Get variable url
-      url <- xml_attr(xml_children(xml_children(var)[
-        xml_name(xml_children(var)) == "notes"]), attr = "URI")
+      url <- xml2::xml_attr(xml2::xml_children(xml2::xml_children(var)[
+        xml2::xml_name(xml2::xml_children(var)) == "notes"]), attr = "URI")
       if (length(url) > 0) attr(data[[varname]], "url") <-
         url else attr(data[[varname]], "url") <- ""
       
       #Get variable value labels
-      varlabel_nodes <- xml_children(var)[
-        xml_name(xml_children(var)) == "catgry"]
+      varlabel_nodes <- xml2::xml_children(var)[
+        xml2::xml_name(xml2::xml_children(var)) == "catgry"]
       if (length(varlabel_nodes) > 0) {
         varlabels <- list()
         for (varlabel in varlabel_nodes) {
           if (length(varlabels) == 0) {
-            varlabels$values <- xml_text(xml_children(varlabel)[
-              xml_name(xml_children(varlabel)) == "catValu"])
-            labels <- xml_children(varlabel)[
-              xml_name(xml_children(varlabel)) == "labl"]
+            varlabels$values <- xml2::xml_text(xml2::xml_children(varlabel)[
+              xml2::xml_name(xml2::xml_children(varlabel)) == "catValu"])
+            labels <- xml2::xml_children(varlabel)[
+              xml2::xml_name(xml2::xml_children(varlabel)) == "labl"]
             for (label in labels){
               if (languages[1] == "all" ||
-                  xml_attr(label, attr = "lang") %in% languages)
-                varlabels[paste0("labels_", xml_attr(label, attr = "lang"))] <-
-                  xml_text(label)
+                  xml2::xml_attr(label, attr = "lang") %in% languages)
+                varlabels[paste0("labels_", xml2::xml_attr(label, attr = "lang"))] <-
+                  xml2::xml_text(label)
             }
           } else {
             varlabels$values <- c(varlabels$values,
-                                  xml_text(xml_children(varlabel)[
-                                    xml_name(xml_children(varlabel)) ==
+                                  xml2::xml_text(xml2::xml_children(varlabel)[
+                                    xml2::xml_name(xml2::xml_children(varlabel)) ==
                                       "catValu"]))
-            labels <- xml_children(varlabel)[
-              xml_name(xml_children(varlabel)) == "labl"]
+            labels <- xml2::xml_children(varlabel)[
+              xml2::xml_name(xml2::xml_children(varlabel)) == "labl"]
             for (label in labels) {
               if (languages[1] == "all" ||
-                  xml_attr(label, attr = "lang") %in% languages)
+                  xml2::xml_attr(label, attr = "lang") %in% languages)
                 varlabels[[paste0(
-                  "labels_", xml_attr(label, attr = "lang"))]] <-
+                  "labels_", xml2::xml_attr(label, attr = "lang"))]] <-
                   c(varlabels[paste0("labels_",
-                                     xml_attr(label, attr = "lang"))][[1]],
-                    xml_text(label))
+                                     xml2::xml_attr(label, attr = "lang"))][[1]],
+                    xml2::xml_text(label))
             }
           }
         }
@@ -303,8 +303,17 @@ read_odf  <-  function(file,
   lang_attr <- names(attributes(data))[c(grep("label", names(attributes(data))),
                                          grep("description",
                                               names(attributes(data))))]
+  for (var in colnames(data)){
+    lang_attr <- c(lang_attr, names(attributes(data[[var]]))[c(grep("label", names(attributes(data[[var]]))),
+                                             grep("description",
+                                                  names(attributes(data[[var]]))))])
+  }
+  
+  lang_attr <- unique(lang_attr)
+  
   langs <- unique(unlist(lapply(lang_attr,
                                 function(x) strsplit(x, "_")[[1]][[2]])))
+  langs <- unique(langs)
   attr(data, "languages") <- langs
   if ("en" %in% langs) lang <- "en" else lang <- langs[1]
   attr(data, "lang") <- lang
